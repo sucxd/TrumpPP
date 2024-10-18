3786f1bb663e27869cee216138b47426d52ff069
sucxd <114672524+sucxd@users.noreply.github.com>
2024-10-18 10:42:06 +0100

    Coffee Spoon Trigger

---
+++ Assets/Scripts/Kris/AltWaysOfMakingCoffee/SpoonCollision.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SpoonCollision : MonoBehaviour
+{
+    public GameObject coffeeBeans;
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (!coffeeBeans.activeSelf && other.CompareTag("CoffeeBeans"))
+        {
+            // Reduce the scale of CoffeeBeans object by 0.001 on the z-axis
+            Vector3 newScale = other.transform.localScale;
+            newScale.z -= 0.001f;
+            other.transform.localScale = newScale;
+
+
+            coffeeBeans.SetActive(true);
+            Debug.Log("Is in trigger");
+        }
+    }
+
+   
+    public void DisableSpoon()
+    {
+        coffeeBeans.SetActive(false); 
+    }
+}
