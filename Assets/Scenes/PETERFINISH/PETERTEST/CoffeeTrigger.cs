5ad6cf578e80518373a6ff8f8d6102466c0317e9
PeterMilchev <petermilchev7@gmail.com>
2024-11-05 22:35:29 +0100

    Few more fixes and changes

--- Assets/Scenes/PETERFINISH/PETERTEST/CoffeeTrigger.cs
+++ Assets/Scenes/PETERFINISH/PETERTEST/CoffeeTrigger.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CoffeeTrigger : MonoBehaviour
 {
-    public AudioSource plantAudio; // Assign the plant's audio in Unity
+    
     public TaskManager task;
     private bool done = false;
 
@@ -10,10 +10,10 @@ public class CoffeeTrigger : MonoBehaviour
     {
         if (other.CompareTag("CoffeeCup") && !done)
         {
-            //plantAudio.Play();
+           
             done = true;
             Debug.Log("Works");
-            task.CompleteTask(1); // Move to the next task after audio is played
+            task.CompleteTask(1); // Move to the next task 
         }
     }
 
