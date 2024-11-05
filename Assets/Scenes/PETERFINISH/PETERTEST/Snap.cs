f4137683d61258a816184da8ea4f95fd8d80f7e9
PeterMilchev <petermilchev7@gmail.com>
2024-11-06 00:33:43 +0100

    Reapply "Reapply "Merge branch 'PeterBranchNew'""
    This reverts commit d45a21468d40815f1396fd9731b7758ca25d91df.

---
+++ Assets/Scenes/PETERFINISH/PETERTEST/Snap.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Snap : MonoBehaviour
+{
+    [SerializeField] private GameObject box1;
+    [SerializeField] private GameObject box2;
+    [SerializeField] private GameObject box3;
+    [SerializeField] private GameObject box4;
+
+    [SerializeField]
+    private GameObject wire;
+
+    private Vector3 pos1;
+    private Rigidbody rb;
+
+    [SerializeField]
+    private DoorController doorController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "1") 
+        {
+            rb.useGravity = false;
+            rb.freezeRotation = true;
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+
+            pos1 = box1.transform.position;
+            snapping(pos1);
+        }
+        if (other.gameObject.tag == "2")
+        {
+            rb.useGravity = false;
+            rb.freezeRotation = true;
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+
+            pos1 = box2.transform.position;
+            snappng(pos1);
+        }
+        if (other.gameObject.tag == "3")
+        {
+            rb.useGravity = false;
+            rb.freezeRotation = true;
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+
+            pos1 = box3.transform.position;
+            snappng(pos1);
+        }
+        if (other.gameObject.tag == "4")
+        {
+            rb.useGravity = false;
+            rb.freezeRotation = true;
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+
+            pos1 = box4.transform.position;
+            snappng(pos1);
+        }
+    }
+
+    private void snappng(Vector3 pos1) 
+    { 
+       wire.transform.position = pos1;
+       doorController.wire1 = false;
+    }
+    private void snapping(Vector3 pos1)
+    {
+        wire.transform.position = pos1;
+        doorController.wire1 = true;
+    }
+
+    public void GrabbedAgain()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+        rb.freezeRotation = false;
+        rb.useGravity = true;
+        doorController.wire1 = false;
+    }
+}
